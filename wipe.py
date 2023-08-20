03d1b9bf6667cc2bff5cbede57fd5834a3097a81
Mahdi Samadi Azar <127014290+mahdisamadiazar@users.noreply.github.com>
2023-08-20 17:37:39 +0330

    Update wipe.py

--- wipe.py
+++ wipe.py
@@ -1,8 +1,18 @@
 import subprocess
+import multiprocessing
 
-disks = ['/dev/sda', '/dev/sdb']  # List of disks to wipe
+command = "lsblk -o NAME -n -d | awk '/^sd/ {print $1}'"
+output = subprocess.check_output(command, shell=True, text=True).splitlines()
 
-for disk in disks:
+dontwipe = "/dev/" + input("Select the disk you don't want to be wiped: ")
+
+disks = []
+for disk in output:
+    if disk != dontwipe:
+        disks.append("/dev/" + disk)
+
+
+def wipe_disk(disk):
     print(f"Wiping {disk}...")
     command = f"sudo dd if=/dev/zero of={disk} bs=4M status=progress"
     subprocess.run(command, shell=True, check=True)    
@@ -14,3 +24,7 @@ for disk in disks:
         file.write(result + "\n")
     print(result)
 
+
+# Create a process pool and map the wipe_disk function to each disk
+with multiprocessing.Pool() as pool:
+    pool.map(wipe_disk, disks)
